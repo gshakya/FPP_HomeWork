d430f045c9e16a6f5d60300b0e5dae6c4dc12042
Gunjan Shakya <gr.skya@gmail.com>
2015-12-03 15:18:34 -0600

    Lesson 6 changed and lesson 7 question 1 added

--- src/week2/lesson6/Question2.java
+++ src/week2/lesson6/Question2.java
@@ -1,10 +1,14 @@
 package week2.lesson6;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Question2 {
@@ -15,8 +19,8 @@ public class Question2 {
 
 	public Question2() {
 
-		width = 200;
-		height = 200;
+		width = 100;
+		height = 100;
 
 		redBtn = new JButton();
 		orangeBtn = new JButton();
@@ -32,16 +36,16 @@ public class Question2 {
 		topFrame.setVisible(true);
 		topFrame.add(topPanel);
 
-		topFrame.setSize(1400, 200);
-		topPanel.setSize(1400, 200);
+		topFrame.setSize(1000, 200);
+		topPanel.setSize(1000, 200);
 
-		redBtn.setSize(width, height);
-		orangeBtn.setSize(width, height);
-		yellowBtn.setSize(width, height);
-		greenBtn.setSize(width, height);
-		blueBtn.setSize(width, height);
-		indigoBtn.setSize(width, height);
-		violetBtn.setSize(width, height);
+		redBtn.setPreferredSize(new Dimension(width, height));
+		orangeBtn.setPreferredSize(new Dimension(width, height));
+		yellowBtn.setPreferredSize(new Dimension(width, height));
+		greenBtn.setPreferredSize(new Dimension(width, height));
+		blueBtn.setPreferredSize(new Dimension(width, height));
+		indigoBtn.setPreferredSize(new Dimension(width, height));
+		violetBtn.setPreferredSize(new Dimension(width, height));
 
 		topPanel.add(redBtn);
 		topPanel.add(orangeBtn);
@@ -58,8 +62,95 @@ public class Question2 {
 		blueBtn.setBackground(Color.blue);
 		indigoBtn.setBackground(new Color(138, 43, 226));
 		violetBtn.setBackground(new Color(75, 0, 130));
+		
+		
+		redBtn.addActionListener(new RedBtnAction());
+		orangeBtn.addActionListener(new OrangeBtnAction());
+		yellowBtn.addActionListener(new YellowBtnAction());
+		greenBtn.addActionListener(new GreenBtnAction());
+		blueBtn.addActionListener(new BlueBtnAction());
+		indigoBtn.addActionListener(new IndigoBtnAction());
+		violetBtn.addActionListener(new VioletBtnAction());
 
 	}
+
+	private class RedBtnAction extends JOptionPane implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			showMessageDialog(null, " Red signifies passion, vitality, enthusiasm and security");
+
+		}
+
+	}
+
+	private class OrangeBtnAction extends JOptionPane implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			showMessageDialog(null,
+					"Orange represents creativity, practicality, playfulness as well as equilibrium or control.");
+
+		}
+
+	}
+
+	private class YellowBtnAction extends JOptionPane implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			showMessageDialog(null,
+					"Yellow represents  represents clarity of thought, wisdom, orderliness and energy.");
+
+		}
+
+	}
+	
+	private class GreenBtnAction extends JOptionPane implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			showMessageDialog(null,
+					"Green denotes fertility, growth, balance, health and wealth.");
+
+		}
+
+	}
+	
+	private class BlueBtnAction extends JOptionPane implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			showMessageDialog(null,
+					"Blue is associated with Spirituality and Divinity.");
+
+		}
+
+	}
+	
+	private class IndigoBtnAction extends JOptionPane implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			showMessageDialog(null,
+					"Indigo is used for spiritual attainment, psychic abilities, self awareness and enhancement of Intuition");
+
+		}
+
+	}
+	
+	private class VioletBtnAction extends JOptionPane implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			showMessageDialog(null,
+					"Violet can ignite one’s imagination and be an inspiration to artists.");
+
+		}
+
+	}
+	
+	
 
 	public static void main(String[] args) {
 		Question2 q2 = new Question2();
---
+++ src/week2/lesson7/Question1.java
@@ -0,0 +1,182 @@
+package week2.lesson7;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.GridBagConstraints;
+import java.awt.Label;
+import java.awt.Insets;
+import java.awt.Button;
+
+public class Question1 extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField mileTxtField;
+	private JLabel pndLbl;
+	private JTextField pndTxtField;
+	private JLabel galLabel;
+	private JTextField galTxtField;
+	private JLabel fhtLabel;
+	private JTextField fhtTxtField;
+	private JLabel kmLabel;
+	private JLabel kgLabel;
+	private JLabel ltrLabel;
+	private JLabel centLabel;
+	private JLabel mileLabel;
+	private JTextField kmTxtField;
+	private JTextField kgTxtField;
+	private JTextField ltrTxtField;
+	private JTextField centTxtField;
+	private Button calculateBtn;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Question1 frame = new Question1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Question1() {
+		int startX = 20;
+		int startY = 20;
+		int width = 70;
+		int height = 20;
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 250);
+		contentPane = new JPanel();
+		getContentPane().add(contentPane);
+
+		contentPane.setLayout(null);
+
+		mileLabel = new JLabel("Mile: ");
+		mileLabel.setBounds(startX, startY, width, height);
+		contentPane.add(mileLabel);
+
+		startX = startX + (width);
+		mileTxtField = new JTextField(10);
+		mileTxtField.setBounds(startX, startY, width, height);
+		contentPane.add(mileTxtField);
+
+		startX = startX + (width);
+		kmLabel = new JLabel("Kilometer: ");
+		kmLabel.setBounds(startX, startY, width, height);
+		contentPane.add(kmLabel);
+
+		startX = startX + (width);
+		kmTxtField = new JTextField(10);
+		kmTxtField.setBounds(startX, startY, width, height);
+		contentPane.add(kmTxtField);
+
+		startX = 20;
+		startY = startY + 20;
+		pndLbl = new JLabel("Pound: ");
+		pndLbl.setBounds(startX, startY, width, height);
+		contentPane.add(pndLbl);
+
+		startX = startX + (width);
+		pndTxtField = new JTextField(10);
+		pndTxtField.setBounds(startX, startY, width, height);
+		contentPane.add(pndTxtField);
+
+		startX = startX + (width);
+		kgLabel = new JLabel("Kilogram: ");
+		kgLabel.setBounds(startX, startY, width, height);
+		contentPane.add(kgLabel);
+
+		startX = startX + (width);
+		kgTxtField = new JTextField(10);
+		kgTxtField.setBounds(startX, startY, width, height);
+		contentPane.add(kgTxtField);
+
+		startX = 20;
+		startY = startY + 20;
+		galLabel = new JLabel("Gallon: ");
+		galLabel.setBounds(startX, startY, width, height);
+		contentPane.add(galLabel);
+
+		startX = startX + (width);
+		galTxtField = new JTextField(10);
+		galTxtField.setBounds(startX, startY, width, height);
+		contentPane.add(galTxtField);
+
+		startX = startX + (width);
+		ltrLabel = new JLabel("Liter: ");
+		ltrLabel.setBounds(startX, startY, width, height);
+		contentPane.add(ltrLabel);
+
+		startX = startX + (width);
+		ltrTxtField = new JTextField(10);
+		ltrTxtField.setBounds(startX, startY, width, height);
+		contentPane.add(ltrTxtField);
+
+		startX = 20;
+		startY = startY + 20;
+		fhtLabel = new JLabel("Fahrenheit: ");
+		fhtLabel.setBounds(startX, startY, width, height);
+		contentPane.add(fhtLabel);
+
+		startX = startX + (width);
+		fhtTxtField = new JTextField(10);
+		fhtTxtField.setBounds(startX, startY, width, height);
+		contentPane.add(fhtTxtField);
+
+		startX = startX + (width);
+		centLabel = new JLabel("Centigrade: ");
+		centLabel.setBounds(startX, startY, width, height);
+		contentPane.add(centLabel);
+
+		startX = startX + (width);
+		centTxtField = new JTextField(10);
+		centTxtField.setBounds(startX, startY, width, height);
+		contentPane.add(centTxtField);
+		
+		mileLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		kmLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		kgLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		ltrLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		centLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		pndLbl.setHorizontalAlignment(SwingConstants.RIGHT);
+		galLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		fhtLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+
+		startX = (int) (20 + (width * 1.5));
+		startY = startY + 20;
+		calculateBtn = new Button("Calculate");
+		calculateBtn.setBounds(startX, startY, width, height);
+		calculateBtn.addActionListener(evt -> {
+			double mileVal = Double.parseDouble(mileTxtField.getText().trim()) * 1.60934;
+			double kgVal = Double.parseDouble(pndTxtField.getText().trim()) * 0.453592;
+			double ltrVal = Double.parseDouble(galTxtField.getText().trim()) * 3.78541;
+			double centVal = (Double.parseDouble(fhtTxtField.getText().trim()) - 32) / 1.8;
+
+			kmTxtField.setText(String.format("%.3f", mileVal));
+			kgTxtField.setText(String.format("%.3f", kgVal));
+			ltrTxtField.setText(String.format("%.3f", ltrVal));
+			centTxtField.setText(String.format("%.3f", centVal));
+
+		});
+
+		contentPane.add(calculateBtn);
+	}
+
+}
